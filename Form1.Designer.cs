57866a81269ccb0ae4d4439a1a208dfcdfe4ff03
Peb Yog Hmong <87113768+pyhteam@users.noreply.github.com>
2022-02-02 21:50:57 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace FakeIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFake = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(51, 6);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(254, 20);
+            this.txtIP.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Proxy";
+            // 
+            // btnFake
+            // 
+            this.btnFake.Location = new System.Drawing.Point(51, 33);
+            this.btnFake.Name = "btnFake";
+            this.btnFake.Size = new System.Drawing.Size(75, 23);
+            this.btnFake.TabIndex = 2;
+            this.btnFake.Text = "Fake";
+            this.btnFake.UseVisualStyleBackColor = true;
+            this.btnFake.Click += new System.EventHandler(this.btnFake_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 62);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(381, 340);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 471);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnFake);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFake;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using xNet;
+
+namespace FakeIP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void AddCookie(HttpRequest http, string cookie)
+        {
+            var temp = cookie.Split(';');
+            foreach (var item in temp)
+            {
+                var temp2 = item.Split('=');
+                if (temp2.Count() > 1)
+                {
+                    http.Cookies.Add(temp2[0], temp2[1]);
+                }
+            }
+        }
+        string GetData(string url, HttpRequest http = null, string userArgent = "", string cookie = null)
+        {
+            if (http == null)
+            {
+                http = new HttpRequest();
+                http.Cookies = new CookieDictionary();
+            }
+
+            if (!string.IsNullOrEmpty(cookie))
+            {
+                AddCookie(http, cookie);
+            }
+
+            if (!string.IsNullOrEmpty(userArgent))
+            {
+                http.UserAgent = userArgent;
+            }
+            string html = http.Get(url).ToString();
+            return html;
+        }
+        private void btnFake_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpRequest httpRequest = new HttpRequest();
+                httpRequest.Cookies = new CookieDictionary();
+                httpRequest.UserAgent = Http.ChromeUserAgent();
+                httpRequest.Proxy = Socks5ProxyClient.Parse(txtIP.Text.Trim());
+
+                var html = GetData("https://whoer.net/", httpRequest);
+                webBrowser1.DocumentText = html;
+               
+                
+            }
+            catch (Exception ex)
+            {
+
+                webBrowser1.DocumentText = ex.Message;
+            }
+        }
+    }
+}
